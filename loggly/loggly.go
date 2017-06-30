878ebb7cdc9b1a49a41e56b684dd20017e58f3a9
Peter Cornelius <peter@icracked.com>
2017-06-30 14:56:19 -0700

    specify my new package for testing

--- loggly/loggly.go
+++ loggly/loggly.go
@@ -6,7 +6,7 @@ import (
 	"os"
 
 	"github.com/gliderlabs/logspout/router"
-	"github.com/iamatypeofwalrus/logspout-loggly/loggly/adapter"
+	"github.com/aranaea/logspout-loggly/loggly/adapter"
 )
 
 const (
